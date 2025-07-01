553a0b53fcdce781c020c95218ece11741eb7c8c
amir <najiamirdesktop@gmail.com>
2025-07-01 06:49:50 +0330

    update drag drop items

---
+++ src/app/components/todo-list-item/todo-list-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'todo-list-item',
+    templateUrl: './todo-list-item.html',
+    styleUrl: './todo-list-item.css'
+})
+export class TodoItem {
+    
+}
